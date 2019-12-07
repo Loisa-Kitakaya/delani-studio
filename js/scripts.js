774ab40050e96616bfe5f08347d034616b95b53f
Loisa-Kitakaya <loisakitakaya@gmail.com>
2019-12-07 07:56:16 +0300

    |jquery script|-->|creates the general skeleton for the front-end manipulation of the webpage by the user|

---
+++ js/scripts.js
@@ -0,0 +1,63 @@
+//jquery front-end code
+$(document).ready(function() {
+  //function to control the display of text in-place of the what we do icons
+
+  //design space
+  $().click(function() {
+    $().removeClass();
+    $().addClass();
+  });
+  //development-space
+  $().click(function() {
+    $().removeClass();
+    $().addClass();
+  });
+  //product management stage
+  $().click(function() {
+    $().removeClass();
+    $().addClass();
+  });
+
+  //functions to control the display of the project headers on the project images on mouse hoover, in the portfolio section
+
+  //project one
+  $().hoover(function() {
+    $().toggle();
+    $().show();
+  });
+  //project two
+  $().hoover(function() {
+    $().toggle();
+    $().show();
+  });
+  //project three
+  $().hoover(function() {
+    $().toggle();
+    $().show();
+  });
+  //project four
+  $().hoover(function() {
+    $().toggle();
+    $().show();
+  });
+  //project five
+  $().hoover(function() {
+    $().toggle();
+    $().show();
+  });
+  //project six
+  $().hoover(function() {
+    $().toggle();
+    $().show();
+  });
+  //project seven
+  $().hoover(function() {
+    $().toggle();
+    $().show();
+  });
+  //project eight
+  $().hoover(function() {
+    $().toggle();
+    $().show();
+  });
+});
