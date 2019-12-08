7b16b0ddc9a3635cbdf3fdab1d859d0aaa69569a
Loisa-Kitakaya <loisakitakaya@gmail.com>
2019-12-08 17:49:45 +0300

    |jquery script|-->|adds the method animate() which on hover changes the opacity of the project images and goes back to its original opacity if the cursor hovers out|

--- js/scripts.js
+++ js/scripts.js
@@ -39,35 +39,91 @@ $(document).ready(function() {
   //functions to control the display of the project headers on the project images on mouse hoover, in the portfolio section
 
   //project one
-  $("#project-one").hover(function() {
-    $("#project-one-title").toggle("#project-one");
-  });
+  $("#project-one").hover(
+    function() {
+      $("#project-one").animate({ opacity: "0.65" });
+      $("#project-one-title").toggle("#project-one");
+    },
+    function() {
+      $("#project-one").animate({ opacity: "1" });
+      $("#project-one-title").toggle("#project-one");
+    }
+  );
   //project two
-  $("#project-two").hover(function() {
-    $("#project-two-title").toggle("#project-two");
-  });
+  $("#project-two").hover(
+    function() {
+      $("#project-two").animate({ opacity: "0.65" });
+      $("#project-two-title").toggle("#project-two");
+    },
+    function() {
+      $("#project-two").animate({ opacity: "1" });
+      $("#project-two-title").toggle("#project-two");
+    }
+  );
   //project three
-  $("#project-three").hover(function() {
-    $("#project-three-title").toggle("#project-three");
-  });
+  $("#project-three").hover(
+    function() {
+      $("#project-three").animate({ opacity: "0.65" });
+      $("#project-three-title").toggle("#project-three");
+    },
+    function() {
+      $("#project-three").animate({ opacity: "1" });
+      $("#project-three-title").toggle("#project-three");
+    }
+  );
   //project four
-  $("#project-four").hover(function() {
-    $("#project-four-title").toggle("#project-four");
-  });
+  $("#project-four").hover(
+    function() {
+      $("#project-four").animate({ opacity: "0.65" });
+      $("#project-four-title").toggle("#project-four");
+    },
+    function() {
+      $("#project-four").animate({ opacity: "1" });
+      $("#project-four-title").toggle("#project-four");
+    }
+  );
   //project five
-  $("#project-five").hover(function() {
-    $("#project-five-title").toggle("#project-five");
-  });
+  $("#project-five").hover(
+    function() {
+      $("#project-five").animate({ opacity: "0.65" });
+      $("#project-five-title").toggle("#project-five");
+    },
+    function() {
+      $("#project-five").animate({ opacity: "1" });
+      $("#project-five-title").toggle("#project-five");
+    }
+  );
   //project six
-  $("#project-six").hover(function() {
-    $("#project-six-title").toggle("#project-six");
-  });
+  $("#project-six").hover(
+    function() {
+      $("#project-six").animate({ opacity: "0.65" });
+      $("#project-six-title").toggle("#project-six");
+    },
+    function() {
+      $("#project-six").animate({ opacity: "1" });
+      $("#project-six-title").toggle("#project-six");
+    }
+  );
   //project seven
-  $("#project-seven").hover(function() {
-    $("#project-seven-title").toggle("#project-seven");
-  });
+  $("#project-seven").hover(
+    function() {
+      $("#project-seven").animate({ opacity: "0.65" });
+      $("#project-seven-title").toggle("#project-seven");
+    },
+    function() {
+      $("#project-seven").animate({ opacity: "1" });
+      $("#project-seven-title").toggle("#project-seven");
+    }
+  );
   //project eight
-  $("#project-eight").hover(function() {
-    $("#project-eight-title").toggle("#project-eight");
-  });
+  $("#project-eight").hover(
+    function() {
+      $("#project-eight").animate({ opacity: "0.65" });
+      $("#project-eight-title").toggle("#project-eight");
+    },
+    function() {
+      $("#project-eight").animate({ opacity: "1" });
+      $("#project-eight-title").toggle("#project-eight");
+    }
+  );
 });
